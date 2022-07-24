138fda6810ef868311fa9afbd0f62f99991ad4e6
wyliemickelson <wyliecoyote910@gmail.com>
2022-07-24 15:11:11 -0700

    created css for timer

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
 import '/src/sass/index.scss';
+
+const progressBar = document.getElementById('progressBar');
+
+let progressValue = 0;
+let progressEndValue = 100;
+let speed = 10;
+
+let progress = setInterval(() => {
+	progressValue++;
+	progressBar.style.background = `conic-gradient(
+    hsl(0deg, 91%, 71%) ${progressValue * 3.6}deg,
+    hsl(234deg, 39%, 14%) ${progressValue * 3.6}deg
+  )`;
+	if (progressValue == progressEndValue) {
+		clearInterval(progress);
+	}
+}, speed);
