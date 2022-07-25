4d25320fc9066a598b8982cc0ec31e237dda7219
wyliemickelson <wyliecoyote910@gmail.com>
2022-07-24 22:13:26 -0700

    added way to change accent colors using js

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,14 @@
 import '/src/sass/index.scss';
 
+const ACCENT_COLORS = [
+	'hsl(0deg, 91%, 71%)', 
+	'hsl(182deg, 91%, 71%)', 
+	'hsl(284deg, 89%, 74%)'
+];
+
 const progressBar = document.getElementById('progressBar');
 
+let currentAccentColor = getCurrentAccentColor();
 let progressValue = 0;
 let progressEndValue = 100;
 let speed = 10;
@@ -9,10 +16,19 @@ let speed = 10;
 let progress = setInterval(() => {
 	progressValue++;
 	progressBar.style.background = `conic-gradient(
-    hsl(0deg, 91%, 71%) ${progressValue * 3.6}deg,
+    ${currentAccentColor} ${progressValue * 3.6}deg,
     hsl(234deg, 39%, 14%) ${progressValue * 3.6}deg
   )`;
 	if (progressValue == progressEndValue) {
 		clearInterval(progress);
 	}
 }, speed);
+
+function getCurrentAccentColor() {
+	return getComputedStyle(document.documentElement)
+		.getPropertyValue('--clr-accent-current');
+}
+
+function setCurrentAccentColor(color) {
+	document.documentElement.style.setProperty('--clr-accent-current', `${color}`);
+}
